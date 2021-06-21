dcf4ed8429d22da71e4027214260e537fea4ccad
Rob Wing <rob.wing@galvanize.com>
2021-06-21 11:43:20 -0700

    Cleanup

--- src/main/java/com/galvanize/security/SecurityCredentialsConfig.java
+++ src/main/java/com/galvanize/security/SecurityCredentialsConfig.java
@@ -1,7 +1,5 @@
 package com.galvanize.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
